ca63f21c46cfc4faeb89c720608372d71d4e035e
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-30 00:15:56 +0100

    added ThemeProvider and basic theme file

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import reportWebVitals from './reportWebVitals';
 import { HomePage } from './features/HomePage';
 import { Normalize } from 'styled-normalize';
 import { GlobalStyle } from './GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+import { themeLight } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Normalize />
-    <GlobalStyle />
-    <HomePage />
+    <ThemeProvider theme={themeLight}>
+      <Normalize />
+      <GlobalStyle />
+      <HomePage />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+const colors = {
+    mineShaft: "#252525",
+};
+
+const common = {
+    breakpoints: {
+        mobileMax: 767,
+        tabletMax: 1199,
+    },
+};
+
+export const themeLight = {
+    ...common,
+};
