2a3522437de19cec5f5ed61544f2a2c342c10a1f
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-29 22:16:59 +0100

    added Normalize and GlobalStyle to main root

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
 import reportWebVitals from './reportWebVitals';
+import { HomePage } from './features/HomePage';
+import { Normalize } from 'styled-normalize';
+import { GlobalStyle } from './GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Normalize />
+    <GlobalStyle />
+    <HomePage />
   </React.StrictMode>
 );
 
