a9779e8704d122016037ac2356a1412af390cb74
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-30 14:59:33 +0100

    added Wrapper to whole page

--- src/features/HomePage/index.js
+++ src/features/HomePage/index.js
@@ -1,7 +1,10 @@
+import { Wrapper } from "../styled";
 import { MainInformation } from "./MainInformation";
+import { SkillsSection } from "./SkillsSection";
 
 export const HomePage = () => (
-    <>
+    <Wrapper>
         <MainInformation />
-    </>
+        <SkillsSection />
+    </Wrapper>
 );
