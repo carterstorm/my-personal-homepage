d8d0a08b898ea4a967a975efcc96498adc6faab5
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-30 23:24:11 +0100

    edited skills

--- src/currentSkills.js
+++ src/currentSkills.js
@@ -24,71 +24,71 @@ export const currentSkills = [
         skillText: "SASS/LESS",
     },
     {
-        id: 67,
+        id: 7,
         skillText: "Styled-components",
     },
     {
-        id: 7,
+        id: 8,
         skillText: "JavaScript ES6+",
     },
     {
-        id: 8,
+        id: 9,
         skillText: "Immutability",
     },
     {
-        id: 9,
+        id: 10,
         skillText: "React",
     },
     {
-        id: 10,
+        id: 11,
         skillText: "React Router",
     },
     {
-        id: 11,
+        id: 12,
         skillText: "Redux-Saga",
     },
     {
-        id: 12,
+        id: 13,
         skillText: "Redux (Toolkit)",
     },
     {
-        id: 13,
+        id: 14,
         skillText: "React Hooks",
     },
     {
-        id: 14,
+        id: 15,
         skillText: "Error handling",
     },
     {
-        id: 15,
+        id: 16,
         skillText: "Working with API (fetch, axios)",
     },
     {
-        id: 16,
+        id: 17,
         skillText: "Promises, Async/Await",
     },
     {
-        id: 17,
+        id: 18,
         skillText: "Git & GitHub",
     },
     {
-        id: 18,
+        id: 19,
         skillText: "Teamwork",
     },
     {
-        id: 19,
+        id: 20,
         skillText: "Markdown",
     },
     {
-        id: 20,
+        id: 21,
         skillText: "Trello",
     },
     {
-        id: 21,
+        id: 22,
         skillText: "Scrum",
     },
     {
-        id: 22,
+        id: 23,
         skillText: "NPM",
     },
 ];
--- src/nextSkills.js
+++ src/nextSkills.js
@@ -12,59 +12,63 @@ export const nextSkills = [
         skillText: "NX",
     },
     {
-        id: 34,
+        id: 4,
         skillText: "pnpm",
     },
     {
-        id: 4,
+        id: 5,
         skillText: "Tailwind CSS",
     },
     {
-        id: 5,
+        id: 6,
         skillText: "Storybook",
     },
     {
-        id: 56,
+        id: 7,
         skillText: "Chromatica",
     },
     {
-        id: 6,
+        id: 8,
         skillText: "Unit testing",
     },
     {
-        id: 7,
+        id: 9,
         skillText: "React Testing Library",
     },
     {
-        id: 8,
+        id: 10,
         skillText: "Next.js",
     },
     {
-        id: 9,
+        id: 11,
         skillText: "Node.js",
     },
     {
-        id: 9,
+        id: 12,
         skillText: "NestJS",
     },
     {
-        id: 10,
+        id: 13,
         skillText: "MJML",
     },
     {
-        id: 11,
+        id: 14,
         skillText: "Zod",
     },
     {
-        id: 12,
+        id: 15,
         skillText: "Automation and AI tools",
     },
     {
-        id: 13,
+        id: 16,
         skillText: "RxJS",
     },
     {
-        id: 14,
+        id: 17,
         skillText: "Make",
+    },
+    {
+        id: 18,
+        skillText: "SQL",
     },
 ];
