a09ad3f6a5e3333d88feb37507cc0633eadca890
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-30 14:58:09 +0100

    added ForwardProp

--- src/index.js
+++ src/index.js
@@ -4,17 +4,20 @@ import reportWebVitals from './reportWebVitals';
 import { HomePage } from './features/HomePage';
 import { Normalize } from 'styled-normalize';
 import { GlobalStyle } from './GlobalStyles';
-import { ThemeProvider } from 'styled-components';
+import { StyleSheetManager, ThemeProvider } from 'styled-components';
 import { themeLight } from './theme';
+import isPropsValid from '@emotion/is-prop-valid'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={themeLight}>
-      <Normalize />
-      <GlobalStyle />
-      <HomePage />
-    </ThemeProvider>
+    <StyleSheetManager shouldForwardProp={isPropsValid} disableVendorPrefixes={false}>
+      <ThemeProvider theme={themeLight}>
+        <Normalize />
+        <GlobalStyle />
+        <HomePage />
+      </ThemeProvider>
+    </StyleSheetManager>
   </React.StrictMode>
 );
 
