d2ab600e671e04c0bb14ad2f895ef3c44cd384bb
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-30 22:55:17 +0100

    new skills section which I want to learn next

--- src/features/HomePage/index.js
+++ src/features/HomePage/index.js
@@ -2,13 +2,19 @@ import { Wrapper } from "../styled";
 import { MainInformation } from "./MainInformation";
 import { SkillsSection } from "../../common/SkillsSection";
 import { currentSkills } from "../../currentSkills";
+import { nextSkills } from "../../nextSkills";
 
 export const HomePage = () => (
     <Wrapper>
         <MainInformation />
         <SkillsSection
-            title="My skillset"
+            title="My skillset ⚒️"
             skills={currentSkills}
+        />
+        <SkillsSection
+            title="What I want to learn next 📚"
+            skills={nextSkills}
         />
     </Wrapper>
 );
+
---
+++ src/nextSkills.js
@@ -0,0 +1,58 @@
+export const nextSkills = [
+    {
+        id: 1,
+        skillText: "TypeScript",
+    },
+    {
+        id: 2,
+        skillText: "React Context",
+    },
+    {
+        id: 3,
+        skillText: "NX",
+    },
+    {
+        id: 4,
+        skillText: "Tailwind CSS",
+    },
+    {
+        id: 5,
+        skillText: "Storybook",
+    },
+    {
+        id: 6,
+        skillText: "Unit testing",
+    },
+    {
+        id: 7,
+        skillText: "React Testing Library",
+    },
+    {
+        id: 8,
+        skillText: "Next.js",
+    },
+    {
+        id: 9,
+        skillText: "NestJS",
+    },
+    {
+        id: 10,
+        skillText: "MJML",
+    },
+    {
+        id: 11,
+        skillText: "Zod",
+    },
+    {
+        id: 12,
+        skillText: "Automation and AI tools",
+    },
+    {
+        id: 13,
+        skillText: "RxJS",
+    },
+    {
+        id: 14,
+        skillText: "Make",
+    },
+];
