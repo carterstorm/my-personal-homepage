e035150e44ba76f5ba0294fd9faa59cf1f8e34bc
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-30 15:45:07 +0100

    deleted SkillText

--- src/features/HomePage/SkillsSection/styled.js
+++ src/features/HomePage/SkillsSection/styled.js
@@ -2,14 +2,14 @@ import styled from "styled-components";
 
 export const Wrapper = styled.section`
     margin-top: 60px;
-    padding: 0 32px 32px;
+    padding: 0 32px 0;
     background-color: #fff;
 `;
 
 export const SkillsList = styled.ul`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
-    grid-gap: 12px auto;
+    grid-gap: 12px 36px;
     margin: 0;
     padding: 32px 0;
     font-size: 14px;
@@ -23,8 +23,6 @@ export const SkillItem = styled.li`
         content: "👉";
         margin-right: 16px;
         font-size: 20px;
+        vertical-align: -3px;
     };
-`;
-
-export const SkillText = styled.span`
 `;
