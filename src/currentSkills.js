02f7c2d960d1356fe749d470529c03f7c742dd28
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-30 15:45:17 +0100

    added currentSkills

---
+++ src/currentSkills.js
@@ -0,0 +1,90 @@
+export const currentSkills = [
+    {
+        id: 1,
+        skillText: "Semantic HTML",
+    },
+    {
+        id: 2,
+        skillText: "Responsive Web Design",
+    },
+    {
+        id: 3,
+        skillText: "CSS BEM convention",
+    },
+    {
+        id: 4,
+        skillText: "CSS Grid",
+    },
+    {
+        id: 5,
+        skillText: "CSS Flexbox",
+    },
+    {
+        id: 6,
+        skillText: "SASS/LESS",
+    },
+    {
+        id: 7,
+        skillText: "JavaScript ES6+",
+    },
+    {
+        id: 8,
+        skillText: "Immutability",
+    },
+    {
+        id: 9,
+        skillText: "React",
+    },
+    {
+        id: 10,
+        skillText: "React Router",
+    },
+    {
+        id: 11,
+        skillText: "Redux-Saga",
+    },
+    {
+        id: 12,
+        skillText: "Redux (Toolkit)",
+    },
+    {
+        id: 13,
+        skillText: "React Hooks",
+    },
+    {
+        id: 14,
+        skillText: "Error handling",
+    },
+    {
+        id: 15,
+        skillText: "Working with API (fetch, axios)",
+    },
+    {
+        id: 16,
+        skillText: "Promises, Async/Await",
+    },
+    {
+        id: 17,
+        skillText: "Git & GitHub",
+    },
+    {
+        id: 18,
+        skillText: "Teamwork",
+    },
+    {
+        id: 19,
+        skillText: "Markdown",
+    },
+    {
+        id: 20,
+        skillText: "Trello",
+    },
+    {
+        id: 21,
+        skillText: "Scrum",
+    },
+    {
+        id: 22,
+        skillText: "NPM",
+    },
+];
