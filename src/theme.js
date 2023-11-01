d8615cc270e1db8fe856f1fa34625d636c4486c0
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-11-01 01:15:04 +0100

    added footer links color

--- src/theme.js
+++ src/theme.js
@@ -26,7 +26,7 @@ export const themeLight = {
         backgroundColor: colorsName.whiteLilac,
         horizontalLine: colorsName.iron,
         listItemCircle: colorsName.scienceBlue,
-        emailFooterColor: colorsName.scienceBlue,
+        footerLinksColor: colorsName.scienceBlue,
         text: {
             primaryTextColor: colorsName.slateGray,
             secondaryTextColor: colorsName.mineShaft,
