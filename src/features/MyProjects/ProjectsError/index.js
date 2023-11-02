0cc76db821fce78e477f1ef56c2dd580e2e10217
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-11-02 21:20:13 +0100

    added gitHub: error, loading & success components

---
+++ src/features/MyProjects/ProjectsError/index.js
@@ -0,0 +1,3 @@
+export const ProjectsError = () => (
+    <>Error</>
+);
--- src/features/MyProjects/index.js
+++ src/features/MyProjects/index.js
@@ -1,3 +1,7 @@
+import { useGitHubData } from "../../useGitHubData";
+import { ProjectsLoading } from "./ProjectsLoading";
+import { ProjectsError } from "./ProjectsError";
+import { ProjectsSuccess } from "./ProjectsSuccess";
 import {
     PortfolioHeading,
     PortfolioIcon,
@@ -6,12 +10,27 @@ import {
     Wrapper
 } from "./styled";
 
-export const MyProjects = () => (
-    <Wrapper>
-        <PortfolioInfo>
-            <PortfolioIcon />
-            <PortfolioHeading>Portfolio</PortfolioHeading>
-            <PortfolioText>My recent projects</PortfolioText>
-        </PortfolioInfo>
-    </Wrapper>
-);
+export const MyProjects = () => {
+    const gitHubData = useGitHubData(2);
+
+    return (
+        <Wrapper>
+            <PortfolioInfo>
+                <PortfolioIcon />
+                <PortfolioHeading>Portfolio</PortfolioHeading>
+                <PortfolioText>My recent projects</PortfolioText>
+            </PortfolioInfo>
+            {
+                gitHubData.state === "loading" ?
+                    (<ProjectsLoading />)
+                    :
+                    gitHubData.state === "error" ?
+                        (<ProjectsError />)
+                        :
+                        (<ProjectsSuccess
+                            gitHubData={gitHubData}
+                        />)
+            }
+        </Wrapper>
+    );
+};
