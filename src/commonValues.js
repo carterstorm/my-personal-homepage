08069101c2278de66f53046378a2fcfe7b8d3a7d
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-11-03 14:21:48 +0100

    added gitHub account link to commonValues

--- src/commonValues.js
+++ src/commonValues.js
@@ -2,3 +2,5 @@ export const email = "sebastian.komorowski1@outlook.com";
 
 export const username = "carterstorm";
 export const gitHubBase = "https://api.github.com";
+
+export const gitHubAccount = "https://github.com/carterstorm";
