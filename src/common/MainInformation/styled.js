10506ed6eec0c6ba73529809a2f705c2ac9a5934
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-11-03 14:21:15 +0100

    moved styles to ButtonLink in common folder

--- src/common/MainInformation/styled.js
+++ src/common/MainInformation/styled.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { ReactComponent as Envelope } from "../../assets/envelope.svg";
 
 export const Wrapper = styled.header`
     display: grid;
@@ -65,41 +64,4 @@ export const Hi = styled.span`
 
 export const Summary = styled(Hi)`
     max-width: 575px;
-`;
-
-export const ButtonLink = styled.a`
-    display: inline-flex;
-    justify-content: space-between;
-    align-items: center;
-    text-align: center;
-    color: ${({ theme }) => theme.colors.button.textColor};
-    border: 2px solid ${({ theme }) => theme.colors.button.borderColor};
-    background-color: ${({ theme }) => theme.colors.button.background};
-    border-radius: ${({ theme }) => theme.borderRadius}px;
-    padding: 6px 18px;
-    font-weight: 700;
-    cursor: pointer;
-    transition: .2s;
-    text-decoration: none;
-    user-select: none;
-
-    &:hover {
-        filter: brightness(110%);
-    };
-
-    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-        font-size: 12px;
-    };
-`;
-
-export const ButtonIcon = styled(Envelope)`
-    width: 24px;
-    height: 24px;
-    margin-right: 8px;
-    color: ${({ theme }) => theme.colors.button.textColor};
-
-    @media(max-width: ${({ theme }) => theme.breakpoints.mobileSmall}px) {
-        width: 20px;
-        height: 20px;
-    };
 `;
