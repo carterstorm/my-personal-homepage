3d313ebf723a62599674adca2dd6b1d86f828bc7
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-11-03 01:34:32 +0100

    added and styled ProjectsSuccess elements

--- src/features/MyProjects/ProjectsSuccess/index.js
+++ src/features/MyProjects/ProjectsSuccess/index.js
@@ -1,3 +1,47 @@
-export const ProjectsSuccess = () => (
-    <>Success</>
+import {
+    ProjectWrapper,
+    ProjectDescription,
+    ProjectItem,
+    ProjectList,
+    ProjectName,
+    LinksContainer,
+    LinkElement,
+    Link,
+} from "./styled";
+
+export const ProjectsSuccess = ({ data }) => (
+    <ProjectWrapper>
+        <ProjectList >
+            {data.map(({
+                id,
+                description,
+                name,
+                html_url
+            }) => (
+                <ProjectItem key={id}>
+                    <ProjectName>{name}</ProjectName>
+                    <ProjectDescription>{description ? description : "Example text"}</ProjectDescription>
+                    <LinksContainer>
+                        <LinkElement>
+                            <span>Link to repo:</span>
+                            <Link
+                                href={html_url}
+                                target="_blank"
+                                title="Link to repo">
+                                {html_url}
+                            </Link>
+                        </LinkElement>
+                        <LinkElement>
+                            <span>Demo:</span>
+                            <Link
+                                href={`https://carterstorm.github.io/${name}/`}
+                                target="_blank"
+                                title="Link to demo">{`https://carterstorm.github.io/${name}/`}
+                            </Link>
+                        </LinkElement>
+                    </LinksContainer>
+                </ProjectItem>
+            ))}
+        </ProjectList>
+    </ProjectWrapper>
 );
---
+++ src/features/MyProjects/ProjectsSuccess/styled.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export const ProjectWrapper = styled.div`
+    margin: 40px 0 0;
+`;
+
+export const ProjectList = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: grid;
+    grid-gap: 32px;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: 300px;
+`;
+
+export const ProjectItem = styled.li`
+    padding: 50px;
+    background-color: ${({ theme }) => theme.colors.tileColor};
+    box-shadow: ${({ theme }) => theme.boxShadow};
+    border-radius: ${({ theme }) => theme.borderRadius}px;
+    display: grid;
+    grid-gap: 24px;
+`;
+
+export const ProjectName = styled.h4`
+    margin: 0;
+    font-size: 24px;
+    font-weight: 700;
+    letter-spacing: 1.2px;
+    color: ${({ theme }) => theme.colors.footerLinksColorHover};
+`;
+
+export const ProjectDescription = styled.p`
+    margin: 0;
+    font-size: 14px;
+    font-weight: 400;
+    letter-spacing: 0.9px;
+`;
+
+export const LinksContainer = styled.div`
+    font-size: 14px;
+`;
+
+export const LinkElement = styled.div`
+    display: grid;
+    grid-template-columns: 100px 1fr;
+    margin: 10px 0;
+`;
+
+export const Link = styled.a`
+    line-height: 1.4;
+    color: ${({ theme }) => theme.colors.text.primaryTextColor};
+    transition: .3s;
+
+    &:hover {
+        color: ${({ theme }) => theme.colors.footerLinksColorHover};
+    };
+`;
