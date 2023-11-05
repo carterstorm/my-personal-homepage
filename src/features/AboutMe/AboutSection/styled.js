30f1fab031e31c45f265763d985c56fb3e8689ec
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-11-05 23:38:14 +0100

    changed styles for tablet & mobile at Article elem

--- src/features/AboutMe/AboutSection/styled.js
+++ src/features/AboutMe/AboutSection/styled.js
@@ -4,11 +4,23 @@ export const Article = styled.article`
     padding: 32px 0;
     font-size: 14px;
     border-top: 1px solid ${({ theme }) => theme.colors.horizontalLine};
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.tabletExtraBreakpoint}px) {
+        padding: 32px;
+    };
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileSmall}px) {
+        padding: 16px;
+    };
 `;
 
 export const Paragraph = styled.p`
     line-height: 1.5;
-    margin: 10px 0;
-    text-indent: 20px;
+    text-indent: 16px;
     text-align: justify;
+    margin: 0;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        font-size: 12px;
+    };
 `;
