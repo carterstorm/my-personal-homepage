f8c0feaab8224450f2759b70b7aaf157ea085330
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-11-08 20:25:06 +0100

    added Education section

---
+++ src/features/AboutMe/Education/index.js
@@ -0,0 +1,30 @@
+import { Paragraph } from "../AboutSection/styled";
+
+export const Education = () => (
+    <>
+        <Paragraph>
+            Regarding my experience and education, I graduated engineering studies at public university.
+            I worked as an automatic line designer for almost 3 years, gaining valuable experience in machine
+            design, creating engineering documentation, project management and working with clients. I started
+            learning Frontend almost 1,5 years ago. My job as a constructor often involved business trips,
+            overtime and weekend work, which made it difficult for me to continue my education. Whenever I could
+            I took time to develop my skills. I didn't give up. However, over the course of my career, I have
+            realized that my true passion was Frontend technologies. I decided to change the industry to develop
+            in a field that gives me real pleasure and satisfaction.
+        </Paragraph>
+        <Paragraph>
+            After some time, to improve my systematic approach,
+            I decided to sign up for an online programming school,
+            which was created by a friend of my friend 😊 who has several years of experience as a Frontend Developer.
+            I can confidently say that after joining this school, I have been learning Frontend technologies with
+            enthusiasm and commitment for one year. The school offers an excellent community of students who support
+            and learn from each other. Under the supervision of mentors, I created two simple applications
+            - currency calculator and todo's list on my own. At the same time during school, I created a weather application
+            and Nike homepage on my own. After a break due to my job, I decided to create the Nike website again.
+            This time, I managed to create a more accurate, fully responsive website that closely resembled the
+            original one. At the end of school, I received a project for a movie and actor browser to code myself,
+            which was created in Figma. During this project, I gained very valuable experience and self-confidence
+            that I can handle similar projects 😊
+        </Paragraph>
+    </>
+);
