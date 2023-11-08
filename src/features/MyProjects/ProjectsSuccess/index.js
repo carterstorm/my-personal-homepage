7afcdac9e11a4135ec55f2389471a812ad10119e
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-11-09 00:20:46 +0100

    changed logic to not display repo without demo

--- src/features/MyProjects/ProjectsSuccess/index.js
+++ src/features/MyProjects/ProjectsSuccess/index.js
@@ -7,41 +7,50 @@ import {
     LinkElement,
     Link,
     Wrapper,
+    LinkSpan,
 } from "./styled";
 
 export const ProjectsSuccess = ({ data }) => (
     <Wrapper>
         <ProjectList >
-            {data.map(({
-                id,
-                description,
-                name,
-                html_url
-            }) => (
-                <ProjectItem key={id}>
-                    <ProjectName>{name}</ProjectName>
-                    <ProjectDescription>{description ? description : "Example text"}</ProjectDescription>
-                    <LinksContainer>
-                        <LinkElement>
-                            <span>Link to repo:</span>
-                            <Link
-                                href={html_url}
-                                target="_blank"
-                                title="Link to repo">
-                                {html_url}
-                            </Link>
-                        </LinkElement>
-                        <LinkElement>
-                            <span>Demo:</span>
-                            <Link
-                                href={`https://carterstorm.github.io/${name}/`}
-                                target="_blank"
-                                title="Link to demo">{`https://carterstorm.github.io/${name}/`}
-                            </Link>
-                        </LinkElement>
-                    </LinksContainer>
-                </ProjectItem>
-            ))}
+            {data.map(({ id, description, name, html_url, homepage }) => {
+                if (name === "carterstorm") {
+                    return null;
+                } else {
+                    return (
+                        <ProjectItem key={id}>
+                            <ProjectName>{name}</ProjectName>
+                            <ProjectDescription>
+                                {description ?
+                                    description :
+                                    `There is no text for this project 🧐 Probably, this project is in progress.
+                                    As soon as the project is completed, its description will appear here 😇`}
+                            </ProjectDescription>
+                            <LinksContainer>
+                                <LinkElement>
+                                    <LinkSpan>Link to repo:</LinkSpan>
+                                    <Link
+                                        href={html_url}
+                                        target="_blank"
+                                        title={`Link to repo: ${name}`}>
+                                        {html_url}
+                                    </Link>
+                                </LinkElement>
+                                {homepage ? (
+                                    <LinkElement>
+                                        <LinkSpan>Link to demo:</LinkSpan>
+                                        <Link
+                                            href={homepage}
+                                            target="_blank"
+                                            title={`Link to demo: ${name}`}>{homepage}
+                                        </Link>
+                                    </LinkElement>
+                                ) : null}
+                            </LinksContainer>
+                        </ProjectItem>
+                    )
+                }
+            })}
         </ProjectList>
     </Wrapper>
 );
